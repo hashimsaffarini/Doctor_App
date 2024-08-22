f9dbac9247a2cf896c2c5bba6c53bb23dc6bf6de
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-22 17:34:11 +0300

    chore: Add spacing helper functions and login page widgets

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,10 +1,72 @@
+import 'package:doc_app/core/helpers/spacing.dart';
+import 'package:doc_app/core/theming/styles.dart';
+import 'package:doc_app/core/widgets/app_text_button.dart';
+import 'package:doc_app/features/login/ui/widgets/dont_have_account.dart';
+import 'package:doc_app/features/login/ui/widgets/email_and_password.dart';
+import 'package:doc_app/features/login/ui/widgets/terms_and_condations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back!',
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Column(
+                  children: [
+                    const EmailAndPassword(),
+                    Align(
+                      //for alignment of the text when arabic language or any other language is selected
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyles.font13BlueRegular,
+                      ),
+                    ),
+                    verticalSpace(40),
+                    AppTextButton(
+                      buttonText: 'Login',
+                      textStyle: TextStyles.font16WhiteSemiBold,
+                      onPressed: () {
+                        // validateThenLogin(context);
+                      },
+                    ),
+                    verticalSpace(16),
+                    const TermsAndConditionsText(),
+                    verticalSpace(60),
+                    const DontHaveAccountText(),
+                    // const LoginBlocListener(),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  // void validateThenLogin(BuildContext context) {
+  //   if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+  //     context.read<LoginCubit>().emitLoadingStates();
+  //   }
+  // }
 }
---
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -0,0 +1,101 @@
+import 'package:doc_app/core/helpers/app_regex.dart';
+import 'package:doc_app/core/helpers/spacing.dart';
+import 'package:doc_app/core/widgets/app_text_form_filed.dart';
+import 'package:doc_app/features/login/ui/widgets/password_validation.dart';
+import 'package:flutter/material.dart';
+
+class EmailAndPassword extends StatefulWidget {
+  const EmailAndPassword({super.key});
+
+  @override
+  State<EmailAndPassword> createState() => _EmailAndPasswordState();
+}
+
+class _EmailAndPasswordState extends State<EmailAndPassword> {
+  bool hasLowerCase = false;
+  bool hasUpperCase = false;
+  bool hasSpecialCharacters = false;
+  bool hasNumber = false;
+  bool hasMinLength = false;
+  bool isObscureText = true;
+  late TextEditingController passwordController;
+  @override
+  void initState() {
+    super.initState();
+    // passwordController = context.read<LoginCubit>().passwordController;
+    // setUpPasswordListener();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    passwordController.dispose();
+  }
+
+  // void setUpPasswordListener() {
+  //   passwordController.addListener(() {
+  //     final password = passwordController.text;
+  //     setState(() {
+  //       hasLowerCase = AppRegex.hasLowerCase(password);
+  //       hasUpperCase = AppRegex.hasUpperCase(password);
+  //       hasSpecialCharacters = AppRegex.hasSpecialCharacter(password);
+  //       hasNumber = AppRegex.hasNumber(password);
+  //       hasMinLength = AppRegex.hasMinLength(password);
+  //     });
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      // key: context.read<LoginCubit>().formKey,
+      child: Column(
+        children: [
+          AppTextFormField(
+            hintText: 'Email',
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isEmailValid(value)) {
+                return 'Please enter a valid email';
+              }
+            },
+            // controller: context.read<LoginCubit>().emailController,
+          ),
+          verticalSpace(18),
+          AppTextFormField(
+            hintText: 'Password',
+            isObscureText: isObscureText,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isObscureText = !isObscureText;
+                });
+              },
+              child: Icon(
+                isObscureText ? Icons.visibility_off : Icons.visibility,
+              ),
+            ),
+            // controller: context.read<LoginCubit>().passwordController,
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isPasswordValid(value)) {
+                return 'Please enter a valid password';
+              }
+            },
+          ),
+          verticalSpace(24),
+          PasswordValidations(
+            hasLowerCase: hasLowerCase,
+            hasUpperCase: hasUpperCase,
+            hasSpecialCharacters: hasSpecialCharacters,
+            hasNumber: hasNumber,
+            hasMinLength: hasMinLength,
+          ),
+          verticalSpace(24),
+        ],
+      ),
+    );
+  }
+}
