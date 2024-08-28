79270bc5bf82e23813dad31deb96b9d9d76a85a0
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-28 19:28:00 +0300

    finish models and repos for sign up

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:doc_app/core/networking/api_constants.dart';
 import 'package:doc_app/features/login/data/models/login_request_body.dart';
 import 'package:doc_app/features/login/data/models/login_response.dart';
+import 'package:doc_app/features/sign_up/data/models/sign_up_request_body.dart';
+import 'package:doc_app/features/sign_up/data/models/sign_up_response.dart';
 import 'package:retrofit/retrofit.dart';
 part 'api_service.g.dart';
 
@@ -12,5 +14,10 @@ abstract class ApiService {
   @POST(ApiConstants.login)
   Future<LoginResponse> login(
     @Body() LoginRequestBody loginRequestBody,
+  );
+
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody signupRequestBody,
   );
 }
